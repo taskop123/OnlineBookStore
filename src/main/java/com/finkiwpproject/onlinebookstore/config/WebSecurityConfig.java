7c691008bf8de101406d2965575cf9bb35efe19a
taskop123 <pavlovtasko@gmail.com>
2021-02-03 19:54:01 +0100

    Initial commit

---
+++ src/main/java/com/finkiwpproject/onlinebookstore/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.finkiwpproject.onlinebookstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+            .antMatchers("/**", "/").permitAll();
+    }
+}
